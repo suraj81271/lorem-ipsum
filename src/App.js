27b355f53772f54ce90dce3dfca1e74c48a42b77
Suraj <surajsingh81271@gmail.com>
2021-04-01 14:52:49 +0530

    given condition that if paragraph limit is above 8 then it will show all 8 paragraphs and if para limit is less then or equal to zero, then it will show 1  para

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (amount > 8) {
+      amount = 8;
+    }
+    if (amount <= 0) {
+      amount = 1;
+    }
+    setText(data.slice(0, amount));
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum? let's generate here</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount"> paragraphs:</label>
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, id) => {
+          return <p key={id}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
