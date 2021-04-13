ad88c8447b203b101b7dd43b4da542334430e65d
Suraj <surajsingh81271@gmail.com>
2021-04-13 18:50:59 +0530

    Updates

---
+++ precache-manifest.ad5edf0958554759c0d169f7bb4db7c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c52035ad39e832c65c3d64f3d44ec3f",
+    "url": "/lorem-ipsum/index.html"
+  },
+  {
+    "revision": "cc7760c8473159a9e1af",
+    "url": "/lorem-ipsum/static/css/main.9d994fcb.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/lorem-ipsum/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lorem-ipsum/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7760c8473159a9e1af",
+    "url": "/lorem-ipsum/static/js/main.3d78c055.chunk.js"
+  },
+  {
+    "revision": "c4ba6ab66839d64706a0",
+    "url": "/lorem-ipsum/static/js/runtime-main.1cd389ed.js"
+  }
+]);
